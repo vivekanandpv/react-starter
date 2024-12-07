36862fd2a2df6ec346c2807dfc97a58907c38bf2
Vivekanand P V <vivekanandpv@gmail.com>
2024-12-07 18:45:44 +0530

    done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div className='p-5'>
+        <h2>React Starter App</h2>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default App;
